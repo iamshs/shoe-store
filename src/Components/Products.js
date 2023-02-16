111625de392134e15a2d64981003daba6c59ebaa
Md Sholayman <mdsholayman998@gmail.com>
2023-02-17 03:56:45 +0600

    animation created for card

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import useProducts from '../hooks/useProducts';
 import Spinner from './Spinner';
+import Plane from '../assets/plane.png'
+import './Style/Products.css'
+import Product from './Product';
+import { motion } from "framer-motion";
+import {container} from '../helpers/framer-motion'
+
 
 const Products = () => {
     const [shoes,isLoading , error] = useProducts()
@@ -9,9 +15,20 @@ const Products = () => {
         return <Spinner />
     }
     return (
-        <div>
-           {shoes.length} 
-        </div>
+        <section className='lg:py-14 py-8 '>
+         <div className='flex items-center justify-start gap-2 px-6'>
+            <img className='w-16' src={Plane} alt='plane' />
+         <h1 className='lg:text-4xl text-2xl font-semibold'>Our Feature</h1>
+         </div>
+         <motion.div 
+          variants={container}
+          initial="hidden"
+          animate="visible"
+         className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6
+          lg:px-14 px-4 place-items-center py-8 lg:py-14'>
+            {shoes.slice(0,8).map((shoe,i) => <Product shoe={shoe} key={i} />)}
+         </motion.div>
+        </section>
     );
 };
 
