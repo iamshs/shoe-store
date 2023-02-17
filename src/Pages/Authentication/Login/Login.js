445d19026b82592b0d8e0d349fc1ff04538dc70a
Md Sholayman <mdsholayman998@gmail.com>
2023-02-17 23:10:11 +0600

    validate email and password

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit , formState: { errors } } = useForm();
   const onSubmit = data => {console.log(data)};
   return (
  <>
@@ -30,8 +30,11 @@ const Login = () => {
               id="email"
               className="w-full border-[1px] border-[#A1A1AA] lg:h-[3.8rem] h-[2.8rem]  
               lg:rounded-[0.7rem] md:rounded-[0.6rem] md:h-[3.5rem] rounded-[0.5rem] my-4 lg:text-[1.4rem] text-[1rem] p-4"
-              {...register("email")}
+              {...register("email", { 
+                required: "Email is required" 
+            })}
             />
+            {errors.email && <p className="text-red-600" role="alert">{errors.email?.message}</p>}
             <label
               className="text-[1rem] text-[#18181B] font-[400]"
               htmlFor="password"
@@ -44,9 +47,13 @@ const Login = () => {
               lg:h-[3.8rem] my-4 lg:text-[1.4rem] text-[1rem] p-4"
               type="password"
               id="password"
-              {...register("password")}
+              {...register("password" , { 
+                required: "Password is required" ,
+                minLength : { value: 6 , message : "Password must be al least 6 character longer" }
+            })}
              
             />
+        {errors.password && <p className="text-red-600" role="alert">{errors.password?.message}</p>}
             <button
            
               className="mt-6 text-[1rem] font-semibold hover:bg-black bg-[#18181B] w-full lg:p-[1.2rem] p-[0.85rem]
